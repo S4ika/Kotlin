e5768cdd3f1511e851d2ca9f17ba1e118061d66c
Ilya Saenko <s41k4gg@gmail.com>
2021-05-10 20:53:44 +0300

    Task 5

--- Laba1.kt
+++ Laba1.kt
@@ -1,10 +1,20 @@
 import java.lang.System.`in`
 import java.util.*
 
+fun liar(lang:String):String =
+    when(lang)
+    {
+        "Kotlin","Prolog" -> "Sycophant"
+        else -> "Good choice"
+    } 
+	
 fun  main (args:Array <String>)
 {
     val scanner = Scanner(`in`)
 	println("Input your name...")
     val name = scanner.next()
+	println("Your favourite language...")
+    val lang = scanner.next()
 	println("Hello, $name")
+	println(liar(lang))
 }
