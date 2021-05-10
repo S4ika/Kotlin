f79ab9e644183faf405c4dd0916715d88bfbc281
Ilya Saenko <s41k4gg@gmail.com>
2021-05-10 19:55:31 +0300

    Create Laba1.kt

---
+++ Laba1.kt
@@ -0,0 +1,10 @@
+import java.lang.System.`in`
+import java.util.*
+
+fun  main (args:Array <String>)
+{
+    val scanner = Scanner(`in`)
+	println("Input your name...")
+    val name = scanner.next()
+	println("Hello, $name")
+}
