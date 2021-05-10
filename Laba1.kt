4a6ab9f78eda62d89b8b910a4835878afc5ad836
Ilya Saenko <s41k4gg@gmail.com>
2021-05-10 20:56:05 +0300

    Task 7

--- Laba1.kt
+++ Laba1.kt
@@ -10,6 +10,15 @@ fun liar(lang:String):String =
 	
 fun sumElem(num:Int) : Int = if(num == 0) 0 else (num % 10)+ sumElem(num / 10)
 
+//Произведение элементов
+fun mulElem(num:Int) : Int = if( num == 0) 1 else (num%10) * mulElem(num/10)
+//Максимальная цифра
+fun max(num:Int) : Int = maxElem(num,0)
+fun maxElem(num:Int,max:Int) : Int = if (num == 0) max else if (num%10 > max) maxElem(num/10,num%10) else maxElem(num/10,max)
+//Минимальное число
+fun min(num:Int) : Int = minElem(num,9)
+fun minElem(num:Int,min:Int) : Int = if (num == 0) min else if (num%10 < min) minElem(num/10,num%10) else minElem(num/10,min)
+
 fun  main (args:Array <String>)
 {
     val scanner = Scanner(`in`)
