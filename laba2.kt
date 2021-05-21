8f9a4b201b0a7ed17f81951f8f66c76130283ee8
S4ika <s4ikait@gmail.com>
2021-05-22 01:06:55 +0300

    Task2

--- laba2.kt
+++ laba2.kt
@@ -2,10 +2,14 @@ import java.lang.System.`in`
 import java.util.*
 //Task 1
 fun sumElem(num:Int) : Int = if (num == 0) 0 else (num%10) + sumElem(num/10)
+//Task 2
+fun sumDown(s:Int):Int = sumElemDown(s,0)
+tailrec fun sumElemDown(num:Int,sum:Int):Int = if (num > 0) sumElemDown(num / 10,(sum + num % 10)) else sum
 
 fun main()
 {
     val scanner = Scanner(`in`)
     var s:Int = scanner.nextInt()
     println("Sum of digits of a number : ${sumElem(s)}")
+	println("Sum of digits of a number : ${sumDown(s)}")
 }
