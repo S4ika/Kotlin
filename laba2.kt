28004a9f718a7dfcdb2e6266d62efccb16f93636
S4ika <s4ikait@gmail.com>
2021-05-22 01:38:47 +0300

    Task 7

--- laba2.kt
+++ laba2.kt
@@ -38,7 +38,39 @@ fun maxDigitIfDown(num : Int) : Int = digitIfDown(num, 0, {a, b -> if (a > b) a 
 fun test1(num : Int) : Int = digitIfDown(num, 1, {a,b -> a * b}, {x -> x % 2 == 0 && x > 6}) // считает произведение четных цифр >6
 fun test2(num : Int) : Int = digitIfDown(num, 1, {a,b -> a + b}, {x -> x in 4..6 }) // считает сумму цифр больших 4 и меньших 7
 fun test3(num : Int) : Int = digitIfDown(num, 1, {a,b -> a + b}, {x -> x == 1}) // считает сумму единиц в числе
+//Task 7
+// 7 Задание
+//7.1
+fun sumProst(num : Int) : Int = sumProstDel(num,0)
 
+tailrec fun primeDigit(digit:Int):Int = if (digit < 2) 0 else prime(digit,2)
+
+tailrec fun prime(digit:Int,iterator:Int):Int = if(digit == iterator) digit else if(digit%iterator != 0) prime(digit,iterator+1) else 0
+
+tailrec fun sumProstDel(num:Int,sum:Int): Int = if (num == 0) sum else {
+    if (primeDigit(num % 10) != 0) sumProstDel(num / 10,sum + primeDigit(num % 10))
+    else sumProstDel(num / 10, sum)
+}
+//7.2
+fun sumOddDigitGreaterThan3(num:Int): Int = sumOddDigit(num,0,3)
+fun oddDigit(digit:Int): Boolean = digit%2 != 0
+tailrec fun sumOddDigit(digit:Int,sum:Int,condition:Int):Int = if(digit==0) sum else{
+    if (digit % 10 > condition && (oddDigit(digit%10)) ) sumOddDigit(digit/10,sum+1,condition)
+    else sumOddDigit(digit/10,sum,condition)
+}
+//7.3
+fun div(num:Int): Int = mulDel(num,1,1)
+fun divisor(num:Int,del:Int):Boolean = num%del==0
+tailrec fun mulDel(num:Int,del:Int,pr:Int): Int =
+    when {
+        num == del -> pr
+        sumElem(del)<sumElem(num) && divisor(num,del)->
+        {   println(del)
+            mulDel(num,del+1,pr*del)
+        }
+        else -> mulDel(num,del+1,pr)
+    }
+	
 fun main()
 {
     val scanner = Scanner(`in`)
